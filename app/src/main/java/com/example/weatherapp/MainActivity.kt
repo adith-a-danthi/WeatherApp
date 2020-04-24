9977a8755183b26017af724347813f616cc172c8
Adith A Danthi <adith.danthi@gmail.com>
2020-04-24 14:01:51 +0530

    MainActivity UI

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private val API_KEY = "af095feda51c14a6152cf7bd32e06667"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 
 }
