044fd011ff154b1aeaecd269fc55d49e051357a6
Adith A Danthi <adith.danthi@gmail.com>
2020-04-26 19:29:56 +0530

    Room DB for Location Data

---
+++ app/src/main/java/com/example/weatherapp/data/source/remote/api/db/WeatherLocationDao.kt
@@ -0,0 +1,21 @@
+package com.example.weatherapp.data.source.remote.api.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.weatherapp.data.source.remote.api.response.WeatherLocation
+
+@Dao
+interface WeatherLocationDao {
+
+    @Insert
+    fun insert(weatherLocation: WeatherLocation)
+
+    @Query("SELECT * FROM weather_data ORDER BY id DESC LIMIT 1")
+    fun getRecentData(): LiveData<WeatherLocation>
+
+    @Delete
+    fun delete(weatherLocation: WeatherLocation)
+}
---
+++ app/src/main/java/com/example/weatherapp/data/source/remote/api/db/WeatherLocationDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.weatherapp.data.source.remote.api.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.weatherapp.data.source.remote.api.response.WeatherLocation
+
+@Database(entities = [WeatherLocation::class], version = 1)
+abstract class WeatherLocationDatabase : RoomDatabase() {
+
+    abstract fun weatherLocationDao(): WeatherLocationDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WeatherLocationDatabase? = null
+
+        internal fun getDatabase(context: Context): WeatherLocationDatabase? {
+            if (INSTANCE == null) {
+                synchronized(Database::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE = Room.databaseBuilder(
+                            context.applicationContext,
+                            WeatherLocationDatabase::class.java,
+                            "weather_db"
+                        ).build()
+                    }
+                }
+            }
+            return INSTANCE
+        }
+
+    }
+
+}
